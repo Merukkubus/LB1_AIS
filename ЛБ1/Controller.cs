857dbab4364ccb3f88edd12ee22dd6660156ab0f
Meruk <Meruk@HOME-PC>
2023-09-22 12:53:00 +0700

    Добавьте файлы проекта.

---
+++ ЛБ1/Controller.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ЛБ1.Reader;
+
+namespace ЛБ1
+{
+    public class Controller
+    {
+        public Controller()
+        {
+            Reader model = new();
+            List<Reader> readers = new List<Reader>();
+        }
+        static void Main(string[] args)
+        {
+            View view = new();
+            while (true)
+            {
+
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        {
+                            return;
+                        }
+                    default:
+                        Console.Clear();
+                        view.Menu();
+                        break;
+                }
+            }
+        }
+        char Delimiter = ',';
+        StringBuilder scv = new StringBuilder();
+        List<Reader> readers = new List<Reader>();
+        public void Add(string path) //case 2
+        {
+            scv.Clear();
+            for (int i = 0; i < readers.Count; i++)
+            {
+                scv.AppendLine(readers[i].id + Delimiter.ToString() + readers[i].name + Delimiter.ToString() + readers[i].surname + Delimiter.ToString()
+                    + readers[i].semester + Delimiter.ToString() + readers[i].scholarship);
+            }
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.Write(scv.ToString());
+            }
+        }
+        public void ClearList()
+        {
+            readers.Clear();
+        }
+        public void AddToList(string path)
+        {
+            using (StreamReader rd = new StreamReader(path))
+            {
+                while (!rd.EndOfStream)
+                {
+                    string str = rd.ReadLine();
+                    string[] onestr = str.Split(new char[] { Delimiter });
+                    if (onestr.Length != 5) { continue; }
+                    readers.Add(new Reader(Convert.ToInt32(onestr[0]), onestr[1], onestr[2],
+                   Convert.ToInt32(onestr[3]), Convert.ToBoolean(onestr[4])));
+                }
+            }
+        }
+        public string GetSeparate(string path, int num)
+        {
+            AddToList(path);
+            return readers[num].GetLine();
+        }
+        public void Delete(string path, int num)
+        {
+            scv.Clear();
+            AddToList(path);
+            readers.RemoveAt(num);
+            for (int i = 0; i < readers.Count; i++)
+            {
+                scv.AppendLine(readers[i].id + Delimiter.ToString() + readers[i].name + Delimiter.ToString() + readers[i].surname + Delimiter.ToString()
+                + readers[i].semester + Delimiter.ToString() + readers[i].scholarship);
+            }
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.Write(scv.ToString());
+            }
+        }
+        public void AddToFile(string path, int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                string user = Convert.ToString(Console.ReadLine());
+                string[] users = user.Split(new char[] { Delimiter });
+                readers.Add(new Reader(Convert.ToInt32(users[0]), users[1], users[2],
+                    Convert.ToInt32(users[3]), Convert.ToBoolean(users[4])));
+            }
+            scv.Clear();
+            for (int i = 0; i < num; i++)
+            {
+                scv.AppendLine(readers[i].id + Delimiter.ToString() + readers[i].name + Delimiter.ToString() + readers[i].surname + Delimiter.ToString()
+                    + readers[i].semester + Delimiter.ToString() + readers[i].scholarship);
+            }
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                sw.Write(scv.ToString());
+            }
+        }
+    }
+}
---
+++ ЛБ1/View.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛБ1
+{
+    class View
+    {
+        Controller controller = new();
+        public void Menu()
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1. Вывод всех записей на экран");
+            Console.WriteLine("2. Вывод записи по номеру");
+            Console.WriteLine("3. Удаление записи (записей) из файла");
+            Console.WriteLine("4. Добавление записи в файл");
+
+            Console.Write("Выберите пункт меню: ");
+            string choice = Console.ReadLine();
+            string path = @"D:\Лабораторные\АИС\ЛБ1\ЛБ1\File.csv";
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Clear();
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Какую запись вывести?");
+                    try
+                    {
+                        Console.WriteLine(controller.GetSeparate(path, Convert.ToInt16(Console.ReadLine()) - 1));
+                        controller.ClearList();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Неверно введены данные.");
+                        controller.ClearList();
+                    }
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Какую запись удалить?");
+                    try
+                    {
+                        controller.Delete(path, Convert.ToInt16(Console.ReadLine()) - 1);
+                        Console.WriteLine("Запись удалена.");
+                        controller.ClearList();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Неверно введены данные.");
+                        controller.ClearList();
+                    }
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("Сколько записей добавить? ");
+                    try
+                    {
+                        controller.AddToFile(path, Convert.ToInt16(Console.ReadLine()));
+                        Console.WriteLine("Запись добавлена");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Неверно введены данные.");
+                    }
+                    controller.ClearList();
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор. Попробуйте еще раз.");
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
